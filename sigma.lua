1c63a948eb840f71405a7b3c0efe5219bd8b3489
adoflgitler <kola.syrn.9@gmail.com>
2025-01-26 02:50:32 +0500

    sigma.lua

--- sigma.lua
+++ sigma.lua
@@ -19,7 +19,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("VoteFo
 game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
 
     end)
-  example:AddButton("Shiny Disco Freddy (3)", function(state)
+  example:AddButton("Shiny Disco Freddy (1)", function(state)
       local args = {
     [1] = "Shiny Disco Freddy",
     [2] = CFrame.new(59.532073974609375, 3.972076416015625, 254.46435546875, 1, 0, 0, 0, 1, 0, 0, 0, 1)
@@ -127,7 +127,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("Spa
 
 game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
 end)
-example:AddButton("Plush Ink King (2)", function(state)
+example:AddButton("Plush Ink King (1)", function(state)
     local args = {
     [1] = "Plush Ink King",
     [2] = CFrame.new(-79.39185333251953, 16.818370819091797, 370.3825378417969, 1, -0.000005665861863235477, -0.00007713301602052525, 0.000005659944690705743, 1, -0.00007671355706406757, 0.0000771334525779821, 0.00007671311323065311, 1)
