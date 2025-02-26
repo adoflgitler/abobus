2e29489ed54edd1cd71c846a4cff1fffb3d0d72e
adoflgitler <kola.syrn.9@gmail.com>
2025-02-26 19:49:35 +0500

    sigma.lua

--- sigma.lua
+++ sigma.lua
@@ -10,7 +10,114 @@ local example = library:CreateWindow({
 game:GetService("ReplicatedStorage"):WaitForChild("Game"):WaitForChild("Functions"):WaitForChild("SummonTower"):InvokeServer(unpack(args))
 
   end)
-  example:AddButton("universal макрос", function(state)
+  example:AddButton("5 макрос", function(state)
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/adoflgitler/vai-sigma-pyhka/refs/heads/main/ваи%20жёстко%20читики.lua', true))()
+    end)
+    example:AddButton("TP UCN", function(state)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-73.59772491455078, 17.536182403564453, 373.1691589355469)
+        
+    end)
+  example:AddButton("SKIP", function(state)
+        game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("VoteSkip"):InvokeServer()
+        
+    end)
+example:AddButton("ExitGame", function(state)
+        game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("ExitGame"):FireServer()
+
+    end)
+    local example = library:CreateWindow({
+      text = "UCN"
+    })
+    example:AddButton("vote for UCN", function(state)
+        local args = {
+    [1] = "UCN"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("VoteForMap"):FireServer(unpack(args))
+
+    end)
+    example:AddButton("Baldi (2)", function(state)
+        local args = {
+    [1] = "Baldi",
+    [2] = CFrame.new(-74.0192642211914, 15.543546676635742, 372.6418151855469, 1, 9.912071163853398e-13, -1.1102230246251565e-16, -1.1546319456101628e-12, 1, 0, -1.1102230246251565e-16, -4.852938971426682e-24, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+
+    end)
+  example:AddButton("Shiny Disco Freddy (1)", function(state)
+      local args = {
+    [1] = "Shiny Disco Freddy",
+    [2] = CFrame.new(-74.0192642211914, 15.543546676635742, 372.6418151855469, 1, 9.912071163853398e-13, -1.1102230246251565e-16, -1.1546319456101628e-12, 1, 0, -1.1102230246251565e-16, -4.852938971426682e-24, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+end)
+example:AddButton("Plush Ink King (2)", function(state)
+    local args = {
+    [1] = "Plush Ink King",
+    [2] = CFrame.new(-79.39185333251953, 16.818370819091797, 370.3825378417969, 1, -0.000005665861863235477, -0.00007713301602052525, 0.000005659944690705743, 1, -0.00007671355706406757, 0.0000771334525779821, 0.00007671311323065311, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+end)
+example:AddButton("Drakobloxxer (2)", function(state)
+    local args = {
+    [1] = "Drakobloxxer",
+    [2] = CFrame.new(-79.49679565429688, 15.543547630310059, 375.76800537109375, 1, -1.5276668818842154e-13, -1.8189894035458565e-12, -2.4868995751603507e-14, 1, 0, -1.8189894035458565e-12, -7.951055210785476e-20, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+end)
+example:AddButton("Huggy Wuggy (3)", function(state)
+    local args = {
+    [1] = "Huggy Wuggy",
+    [2] = CFrame.new(-80.98019409179688, 16.266523361206055, 376.335205078125, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+end)
+example:AddButton("Frostbear (2)", function(state)
+    local args = {
+    [1] = "Frostbear",
+    [2] = CFrame.new(-81.2692642211914, 15.543547630310059, 369.2461242675781, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+end)
+example:AddButton("Blue Friend (2)", function(state)
+    local args = {
+    [1] = "Blue Friend",
+    [2] = CFrame.new(-73.90526580810547, 16.689353942871094, 372.71685791015625, 1, 0.00003956460204790346, -1.1852516745847197e-09, -0.00003956460204790346, 1, -0.00006271028541959822, -1.295855756033859e-09, 0.00006271028541959822, 1)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+end)
+example:AddButton("The Entity (1)", function(state)
+        local args = {
+    [1] = "The Entity",
+    [2] = CFrame.new(-76.95560455322266, 17.72355842590332, 373.2475280761719)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+
+end)
+example:AddButton("Shiny The Entity (1)", function(state)
+        local args = {
+    [1] = "Shiny The Entity",
+    [2] = CFrame.new(-76.955local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Marco8642/science/main/ui%20libs2", true))()
+local example = library:CreateWindow({
+        text = "Misc"
+      }) 
+  example:AddButton("Open all", function(state)
+        local args = {
+    [1] = 10000
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Game"):WaitForChild("Functions"):WaitForChild("SummonTower"):InvokeServer(unpack(args))
+
+  end)
+  example:AddButton("5 макрос", function(state)
         loadstring(game:HttpGet('https://raw.githubusercontent.com/adoflgitler/vai-sigma-pyhka/refs/heads/main/ваи%20жёстко%20читики.lua', true))()
     end)
     example:AddButton("TP UCN", function(state)
@@ -106,6 +213,21 @@ example:AddButton("Shiny The Entity (1)", function(state)
         local args = {
     [1] = "Shiny The Entity",
     [2] = CFrame.new(-76.95560455322266, 17.72355842590332, 373.2475280761719)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+
+end)
+example:AddButton("1x1x1x1 (1)", function(state)
+        local args = {
+    [1] = "1x1x1x1",
+    [2] = CFrame.new(-76.95560455322266, 17.72355842590332, 373.2475280761719)
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
+
+    end)
+60455322266, 17.72355842590332, 373.2475280761719)
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("Functions"):WaitForChild("SpawnTower"):InvokeServer(unpack(args))
